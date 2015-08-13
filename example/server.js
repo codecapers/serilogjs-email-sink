c91b17c567daddb061e0ab220bc0bca05c418611
Ashley Davis <ashley@codecapers.com.au>
2015-08-13 11:41:05 +1000

    Added an example.

---
+++ example/server.js
@@ -0,0 +1,27 @@
+
+var structuredLog = require('structured-log-dev');
+var consoleSink = require('structured-log-dev/console-sink');
+var emailSink = require('../structured-log-email-sink');
+
+var log = structuredLog.configure()
+	.batch()
+	.writeTo(emailSink({
+		transport: {
+			service: "Gmail",
+			auth: {
+				user: '<your-gmail-username>',
+				pass: '<your-gmail-password>',
+			},
+		},
+		email: {
+			to: '<someone>',
+			subject: 'Test Logging',
+		},
+	}))
+    .create();
+
+log.info('Hello this is some information.');
+
+log.warn('This is a warning.');
+
+log.error('This is an error.');
